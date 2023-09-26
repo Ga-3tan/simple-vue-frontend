aebee8963c9474f4390ce3f33a7c620dd2695a7e
Gaétan Zwick <gaetan.zwick.dev@gmail.com>
2023-09-26 14:56:30 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      quote: "",
+    }
+  },
+  methods: {
+    async fetchQuote() {
+      this.quote = await fetch('http://localhost:3000').then(
+          res => res.text()
+      ).catch(
+          err => console.error(err)
+      );
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="fetchQuote()">quote from backend</button>
+  <p>{{ quote }}</p>
+</template>
